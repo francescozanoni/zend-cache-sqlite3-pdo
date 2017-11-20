a8d4df907cf1c14fbcd4252c5872a55590ef20f2
Francesco Zanoni <francescozanoni81@gmail.com>
2017-11-20 21:51:03 +0100

    continued first test creation

--- test/CacheTest.php
+++ test/CacheTest.php
@@ -1,52 +1,27 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 // https://framework.zend.com/manual/1.11/en/zend.test.phpunit.db.html
-class CacheTest extends Zend_Test_PHPUnit_DatabaseTestCase
+class CacheTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
-    private $_connectionMock;
  
-    /**
-     * Returns the test database connection.
-     *
-     * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
-     */
-    protected function getConnection()
+    public function setUp()
     {
-        if($this->_connectionMock == null) {
-            $connection = Zend_Db::factory(...);
-            $this->_connectionMock = $this->createZendDbConnection(
-                $connection, 'zfunittests'
-            );
-            Zend_Db_Table_Abstract::setDefaultAdapter($connection);
-        }
-        return $this->_connectionMock;
+        // Assign and instantiate in one step:
+        $this->bootstrap = new Zend_Application(
+            'testing',
+            __DIR__ . '/application.ini'
+        );
+        parent::setUp();
     }
- 
-    public function testBugInsertedIntoDatabase()
-    {
-        $bugsTable = new Bugs();
- 
-        $data = array(
-            'created_on'      => '2007-03-22 00:00:00',
-            'updated_on'      => '2007-03-22 00:00:00',
-            'bug_description' => 'Something wrong',
-            'bug_status'      => 'NEW',
-            'reported_by'     => 'garfield',
-            'verified_by'     => 'garfield',
-            'assigned_to'     => 'mmouse',
-        );
- 
-        $bugsTable->insert($data);
- 
-        $ds = new Zend_Test_PHPUnit_Db_DataSet_QueryDataSet(
-            $this->getConnection()
-        );
-        $ds->addTable('zfbugs', 'SELECT * FROM zfbugs');
- 
-        $this->assertDataSetsEqual(
-            $this->createFlatXmlDataSet(dirname(__FILE__)
-                                      . "/_files/bugsInsertIntoAssertion.xml"),
-            $ds
-        );
+    
+    public function testCache() {
+        $cacheManager = $this->bootstrap
+            ->getPluginResource('cachemanager')
+            ->getCacheManager();
+        $userAgentCache = $cacheManager->getCache('cachename');
+
+        $response = $userAgentCache->load('aaa');
+        $userAgentCache->save($response, 'aaa');
     }
     
 }
