692f7d729113032c72e94a5ac1a01accf465cc0c
francesco.zanoni <francesco.zanoni@nubilaria.com>
2017-11-21 13:46:12 +0100

    moved to PHPUnit 5, in order to be able to use Zend's built-in test functionalities fixed test application.ini

--- test/CacheTest.php
+++ test/CacheTest.php
@@ -1,9 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 // https://framework.zend.com/manual/1.11/en/zend.test.phpunit.db.html
+// WHY PHPUnit ^5.0: https://stackoverflow.com/questions/6065730/why-fatal-error-class-phpunit-framework-testcase-not-found-in/42561590#42561590
 class CacheTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
- 
+
     public function setUp()
     {
         // Assign and instantiate in one step:
@@ -13,15 +14,15 @@ class CacheTest extends Zend_Test_PHPUnit_ControllerTestCase
         );
         parent::setUp();
     }
-    
+
     public function testCache() {
         $cacheManager = $this->bootstrap
+            ->getBootstrap()
             ->getPluginResource('cachemanager')
             ->getCacheManager();
         $userAgentCache = $cacheManager->getCache('cachename');
-
         $response = $userAgentCache->load('aaa');
         $userAgentCache->save($response, 'aaa');
     }
-    
+
 }
