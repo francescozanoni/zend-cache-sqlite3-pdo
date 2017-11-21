5904fe846ffafb35ef88c545353533fce7e47c55
francesco.zanoni <francesco.zanoni@nubilaria.com>
2017-11-21 21:31:13 +0100

    finalized first tests

--- test/CacheTest.php
+++ test/CacheTest.php
@@ -1,7 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-// https://framework.zend.com/manual/1.11/en/zend.test.phpunit.db.html
-// WHY PHPUnit ^5.0: https://stackoverflow.com/questions/6065730/why-fatal-error-class-phpunit-framework-testcase-not-found-in/42561590#42561590
+
 class CacheTest extends Zend_Test_PHPUnit_ControllerTestCase
 {
 
@@ -15,14 +14,71 @@ class CacheTest extends Zend_Test_PHPUnit_ControllerTestCase
         parent::setUp();
     }
 
-    public function testCache() {
-        $cacheManager = $this->bootstrap
+    /**
+     * Ensure an uncached item is not available.
+     */
+    public function testUncachedItem()
+    {
+
+        $cache = $this->_getCache();
+
+        $uncachedItemValue = $cache->load('uncached_item_key');
+
+        $this->assertFalse($uncachedItemValue);
+
+    }
+
+    /**
+     * Ensure a cached item is available.
+     */
+    public function testCachedItem()
+    {
+
+        $cache = $this->_getCache();
+
+        $cache->save('cached_item_value', 'cached_item_key');
+        $cachedItemValue = $cache->load('cached_item_key');
+
+        $this->assertEquals('cached_item_value', $cachedItemValue);
+
+    }
+
+    /**
+     * Ensure trying to cache an uncacheable value throws a Zend_Cache_Exception.
+     */
+    public function testUncacheableItem()
+    {
+
+        $cache = $this->_getCache();
+
+        try {
+
+            $cache->save(true, 'uncacheable_item_key');
+
+        } catch (Exception $e) {
+
+            $this->assertInstanceOf('Zend_Cache_Exception', $e);
+            $this->assertEquals(
+                'Datas must be string or set automatic_serialization = true',
+                $e->getMessage()
+            );
+
+        }
+
+    }
+
+    /**
+     * @return Zend_Cache_Core
+     */
+    protected function _getCache()
+    {
+
+        return $this->bootstrap
             ->getBootstrap()
             ->getPluginResource('cachemanager')
-            ->getCacheManager();
-        $userAgentCache = $cacheManager->getCache('cachename');
-        $response = $userAgentCache->load('aaa');
-        $userAgentCache->save($response, 'aaa');
+            ->getCacheManager()
+            ->getCache('cachename');
+
     }
 
 }
